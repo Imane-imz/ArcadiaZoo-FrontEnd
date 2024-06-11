850aba37e0a797ec93e49c03a92a498c16272716
Imane-imz <imanemardhy@gmail.com>
2024-06-11 12:09:27 +0200

    Signins and signups

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,12 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/signinadmin", "Connexion Administrateur", "/pages/signinadmin.html"),
+    new Route("/signinvet", "Connexion Vétérinaire", "/pages/signinvet.html"),
+    new Route("/signinemployee", "Connexion Employé.e", "/pages/signinemployee.html"),
+    new Route("/signup", "Inscription", "/pages/signup.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Zoo Arcadia";
