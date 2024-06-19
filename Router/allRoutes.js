8ac5344def3a995a78a447f65412c839b57c2002
Imane-imz <imanemardhy@gmail.com>
2024-06-19 12:23:56 +0200

    Ajout d'images

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,10 +3,11 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/signinadmin", "Connexion Administrateur", "/pages/signinadmin.html"),
-    new Route("/signinvet", "Connexion Vétérinaire", "/pages/signinvet.html"),
-    new Route("/signinemployee", "Connexion Employé.e", "/pages/signinemployee.html"),
-    new Route("/signup", "Inscription", "/pages/signup.html"),
+    new Route("/signinadmin", "Connexion Administrateur", "/pages/auth/signinadmin.html"),
+    new Route("/signinvet", "Connexion Vétérinaire", "/pages/auth/signinvet.html"),
+    new Route("/signinemployee", "Connexion Employé.e", "/pages/auth/signinemployee.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/admindashboard", "Compte Administrateur", "/pages/auth/admindashboard.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
