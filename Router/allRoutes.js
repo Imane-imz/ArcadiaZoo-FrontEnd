71ab0d0db293e0e004ac05ececa76e152b5797c6
Imane-imz <imanemardhy@gmail.com>
2024-07-10 09:47:34 +0200

    Ajout des pages

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -15,6 +15,18 @@ export const allRoutes = [
     new Route("/lions", "Nos Lions", "/pages/animaux/lions.html"),
     new Route("/avis", "Avis", "/pages/avis.html"),
     new Route("/contact", "Contact", "/pages/contact.html"),
+    new Route("/tigres", "Nos Tigres", "/pages/animaux/tigres.html"),
+    new Route("/elephants", "Nos Éléphants", "/pages/animaux/elephants.html"),
+    new Route("/tortues", "Nos Tortues", "/pages/animaux/tortues.html"),
+    new Route("/loutres", "Nos Loutres", "/pages/animaux/loutres.html"),
+    new Route("/alligators", "Nos Alligators", "/pages/animaux/alligators.html"),
+    new Route("/perroquets", "Nos Perroquets", "/pages/animaux/perroquets.html"),
+    new Route("/flamants", "Nos Flamants", "/pages/animaux/flamants.html"),
+    new Route("/autruches", "Nos Autruches", "/pages/animaux/autruches.html"),
+    new Route("/activites", "Nos Activités", "/pages/activites/activites.html"),
+    new Route("/restaurant", "Notre Restaurant", "/pages/activites/restaurant.html"),
+    new Route("/visiteguidee", "Les Visites Guidées", "/pages/activites/visiteguidee.html"),
+    new Route("/petittrain", "Visites en Petit Train", "/pages/activites/petittrain.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
